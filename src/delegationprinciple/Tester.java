78ea4e56e0bfaded25e53bc5131d25f1be82f95e
Jesus Zuniga <jesus.zuniga@macys.com>
2019-09-24 15:59:44 -0300

    Delegation Principle

---
+++ src/delegationprinciple/Tester.java
@@ -0,0 +1,25 @@
+package delegationprinciple;
+
+class RealPrinter {
+    // this is the delegate
+    public void print() {
+        System.out.println("The delegate is printing");
+    }
+}
+
+class Printer {
+    // this is the delegator
+    private RealPrinter realPrinter = new RealPrinter();
+
+    public void print() {
+        realPrinter.print();
+    }
+}
+
+public class Tester {
+    public static void main(String[] args) {
+        // to the outside world it looks like Printer actually prints
+        Printer printer = new Printer();
+        printer.print();
+    }
+}
