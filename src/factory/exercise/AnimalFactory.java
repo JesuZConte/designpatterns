c08045b2d1dd1faab90e6166f6f1840e6be7d315
Jesus Zuniga <jesus.zuniga@macys.com>
2019-09-26 18:49:03 -0300

    Factory Pattern

--- src/factory/exercise/AnimalFactory.java
+++ src/factory/exercise/AnimalFactory.java
@@ -5,11 +5,10 @@ public class AnimalFactory {
     public static Animal getAnimalType(String animalType) {
         String type = animalType.trim().toLowerCase();
 
+        // do not add a break since the return is the last line executed.
         switch (type) {
             case "duck": return new Duck();
-
             case "tiger": return new Tiger();
-
             default: return null;
         }
     }
