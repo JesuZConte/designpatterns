395c9f612715a7faecd3c2582061f87b7a577bad
Jesus Zuniga <jesus.zuniga@macys.com>
2019-10-08 18:18:45 -0300

    Fixing exercise

--- src/adapter/exercise/AdvancedMediaPlayerAdapter.java
+++ src/adapter/exercise/AdvancedMediaPlayerAdapter.java
@@ -3,17 +3,23 @@ package adapter.exercise;
 public class AdvancedMediaPlayerAdapter implements MediaPlayer{
 
     private AdvancedMediaPlayer advancedMediaPlayer;
+    private MediaPlayer audioPlayer;
 
     @Override
     public void play(String audioType, String fileName) {
-        if(audioType.equalsIgnoreCase("mp4")) {
+        if(audioType == null || audioType.isEmpty() || audioType.equalsIgnoreCase("mp3")) {
+            audioPlayer = new AudioPlayer();
+            audioPlayer.play(audioType, fileName);
+        }else if(audioType.equalsIgnoreCase("mp4")) {
             advancedMediaPlayer = new Mp4Player();
             advancedMediaPlayer.loadFilename(fileName);
             advancedMediaPlayer.listen();
-        } else {
+        } else if(audioType.equalsIgnoreCase("vlc")){
             advancedMediaPlayer = new VlcPlayer();
             advancedMediaPlayer.loadFilename(fileName);
             advancedMediaPlayer.listen();
+        } else {
+            System.out.println("Invalid media. " + audioType + " not supported.");
         }
     }
 }
